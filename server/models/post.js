e1a1a40a7e3a1662dc0aaed9d06de6d6353bdf8c
sakoh <sakohartounian@yahoo.com>
2014-12-06 23:28:25 -0800

    validation added to the post model

--- server/models/post.js
+++ server/models/post.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test');
 
-var Post = mongoose.model('Post', {
-  title: String,
-  body: String
+var postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true }
 });
+
+var Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
